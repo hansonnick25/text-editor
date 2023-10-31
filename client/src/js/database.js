e684ab9acbc4730bb1d2537741b23c2b4623d526
Nick Hanson <hansonnick25@gmail.com>
2023-10-31 16:26:30 -0500

    Adding handles for installing and setup database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,38 @@
-import { openDB } from 'idb';
+import { openDB } from 'idb'
 
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
+        console.log('jate database already exists')
+        return
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true })
+      console.log('jate database created')
     },
-  });
+  })
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async content => {
+  console.log('putDb called')
+  const db = await initdb()
+  const tx = db.transaction('jate', 'readwrite')
+  const store = tx.objectStore('jate')
+  await store.put(content)
+  await tx.done
+  console.log('putDb done')
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('getDb called')
+  const db = await initdb()
+  const tx = db.transaction('jate', 'readonly')
+  const store = tx.objectStore('jate')
+  const content = await store.getAll()
+  await tx.done
+  console.log('getDb done')
+  return content
+}
 
-initdb();
+initdb()
