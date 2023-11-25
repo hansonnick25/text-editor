992c59f52811726dc1d61daaceed0080b64fc80e
Nick Hanson <hansonnick25@gmail.com>
2023-11-25 10:26:43 -0600

    Fix button visibility issue in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,7 +4,7 @@ const butInstall = document.getElementById('buttonInstall')
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', event => {
   window.deferredPrompt = event
-  butInstall.disabled = false
+  butInstall.classList.toggle('hidden', false)
 })
 
 // TODO: Implement a click event handler on the `butInstall` element
@@ -14,10 +14,8 @@ butInstall.addEventListener('click', async () => {
     return
   }
   promptEvent.prompt()
-  const result = await promptEvent.userChoice
-  console.log(result)
   window.deferredPrompt = null
-  butInstall.disabled = true
+  butInstall.classList.toggle('hidden', true)
 })
 
 // TODO: Add an handler for the `appinstalled` event
