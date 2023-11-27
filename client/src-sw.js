171173bde74556015a74e56169340c3983ae7703
Nick Hanson <hansonnick25@gmail.com>
2023-11-27 02:19:56 -0600

    Update service worker caching strategy

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes')
-const { CacheFirst } = require('workbox-strategies')
+const { warmStrategyCache } = require('workbox-recipes')
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies')
 const { registerRoute } = require('workbox-routing')
 const { CacheableResponsePlugin } = require('workbox-cacheable-response')
 const { ExpirationPlugin } = require('workbox-expiration')
@@ -37,6 +37,9 @@ registerRoute(
       // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60, // calculates to 30 days
       }),
     ],
   })
